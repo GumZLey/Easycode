16342a255a558de62adbde414da919b668a9b255
GumZLey <58941219+GumZLey@users.noreply.github.com>
2020-02-25 19:12:18 +0700

    Add files via upload

---
+++ MyIdol.py
@@ -0,0 +1,34 @@
+
+class Idol:
+    
+    def __init__(self,first,middle,last,age,school,university,career,ceo,birthday):
+        self.first = first
+        self.middle = middle
+        self.last = last
+        self.fullname = first + " " + middle+ " " + last
+        self.age = age
+        self.school = school
+        self.university = university
+        self.career = career
+        self.ceo = ceo
+        self.birthday = birthday
+        
+
+#Please pretend that you can't see these line 
+Guess_who = Idol(
+    "Elon",
+    "Reeve",
+    "Musk",
+    "49",
+    "Pretoria Boys High School",
+    "University of Pretoria, Queen's University, University of Pennsylvania",
+    "Zip2, X.com,SpaceX,Tesla",
+    "SpaceX,Tesla",
+    "June 28, 1971"
+)
+#----------------------------------------------------------------------------------------------
+
+#Write Guess_who.first/middle/last/fullname/age/school/university/career/ceo/birthday 
+
+print(Guess_who.)
+    
